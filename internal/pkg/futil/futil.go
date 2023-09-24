b8603edc202fb6d7461f470c95aaae543838b77a
Viktor Persson <viktor.persson@arcsin.se>
2023-09-24 16:41:18 +0200

    Update file utils.

--- internal/pkg/futil/futil.go
+++ internal/pkg/futil/futil.go
@@ -6,14 +6,34 @@ import (
 	"io"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
+func FindFilepathsByExtension(rootDirPath string, ext string) []string {
+	files := []string{}
+
+	filepath.Walk(rootDirPath, func(path string, fileInfo os.FileInfo, err error) error {
+		fileInfo, err = os.Stat(path)
+		if err != nil {
+			return err
+		}
+
+		if fileInfo.Mode().IsRegular() && strings.HasSuffix(path, ext) {
+			files = append(files, path)
+		}
+
+		return nil
+	})
+
+	return files
+}
+
 func DirHasFile(rootDirPath string, filename string) bool {
-	file, _ := FindFileAbsPath(rootDirPath, filename)
+	file, _ := FindFilepath(rootDirPath, filename)
 	return file != ""
 }
 
-func FindFileAbsPath(rootDirPath string, filename string) (string, error) {
+func FindFilepath(rootDirPath string, filename string) (string, error) {
 	file := ""
 
 	filepath.Walk(rootDirPath, func(path string, fileInfo os.FileInfo, err error) error {
@@ -41,7 +61,7 @@ func FindFileAbsPath(rootDirPath string, filename string) (string, error) {
 }
 
 func FindFileSha256(rootDirPath string, filename string) (string, error) {
-	fileAbsPath, err := FindFileAbsPath(rootDirPath, filename)
+	fileAbsPath, err := FindFilepath(rootDirPath, filename)
 
 	if err != nil {
 		return "", err
@@ -50,8 +70,8 @@ func FindFileSha256(rootDirPath string, filename string) (string, error) {
 	return FileSha256(fileAbsPath)
 }
 
-func FileSha256(fileAbsPath string) (string, error) {
-	file, err := os.Open(fileAbsPath)
+func FileSha256(filepath string) (string, error) {
+	file, err := os.Open(filepath)
 	if err != nil {
 		return "", err
 	}
